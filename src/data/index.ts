49e142899bdc7bc1732a52043f5a6d7406a574cd
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-10-28 18:44:45 +0200

    add: initial data

---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+export const dataValues = [
+  {
+    question: "Lorem",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, corrupti voluptatibus quae nam distinctio eos qui enim quis saepe quibusdam eligendi deleniti illum porro facere culpa cupiditate dolor doloremque blanditiis?",
+    id: 1,
+  },
+  {
+    question: "Aorem",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, corrupti voluptatibus quae nam distinctio eos qui enim quis saepe quibusdam eligendi deleniti illum porro facere culpa cupiditate dolor doloremque blanditiis?",
+    id: 2,
+  },
+  {
+    question: "Borem",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, corrupti voluptatibus quae nam distinctio eos qui enim quis saepe quibusdam eligendi deleniti illum porro facere culpa cupiditate dolor doloremque blanditiis?",
+    id: 3,
+  },
+];
